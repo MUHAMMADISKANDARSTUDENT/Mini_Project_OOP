e1126352347b59275ba47c75dfe8a152dc1d35d3
vasun-s251594958 <s251594958@studentmail.unimap.edu.my>
2026-09-14 23:23:16 +0800

    Add User business model class

---
+++ src/model/User.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class User {
+
+    private String userId;
+    private String name;
+    private String email;
+    private String phone;
+
+    public User(String userId, String name, String email, String phone) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getDetails() {
+        return userId + " - " + name + " - " + email + " - " + phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
